7abbec7109c98ec890f2ac83287a86da90aba91b
Bekzod <bekrov21@gmail.com>
2022-09-27 15:50:46 +0500

    before new block

--- js/script.js
+++ js/script.js
@@ -95,3 +95,30 @@ $('.slider_inner').slick({
     prevArrow: false,
     nextArrow: false
 });
+
+// $('.method_card').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 1,
+//     adaptiveHeight: true,
+//     prevArrow: false,
+//     nextArrow: false
+// });
+
+
+// const hamburger = document.querySelector('.hamburger');
+// const navMenu = document.querySelector('.nav_menu');
+//
+// hamburger.addEventListener('click', () => {
+//     hamburger.classList.toggle('active');
+//     navMenu.classList.toggle('active');
+//
+// })
+//
+// document.querySelectorAll('.nav_menu li').forEach(evt => {
+//     evt.addEventListener('click', () => {
+//         hamburger.classList.remove('active');
+//         navMenu.classList.remove('active');
+//     })
+// })
