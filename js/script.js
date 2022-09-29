3e76a302af2eaecfc3ceb8a700f61ac526eba874
Bekzod <bekrov21@gmail.com>
2022-09-29 18:10:04 +0500

    first popup

--- js/script.js
+++ js/script.js
@@ -1,89 +1,3 @@
-// (function () {
-//     let $html = $(document.documentElement),
-//         menuBtn = document.querySelector(".burger"),
-//         menuWrapper = document.querySelector(".menu_burger"),
-//         menuClose = document.querySelector(".menuClose"),
-//         openedMenu = "opened",
-//         overflowHidden = "oveflowHidden";
-//
-//
-//
-//
-//
-//     menuBtn.on("click", function (event) {
-//         menuWrapper.toggleClass(openedMenu);
-//         menuBtn.toggleClass(openedMenu);
-//         $html.toggleClass(overflowHidden);
-//         $html.toggleClass("open_menu");
-//     });
-//     menuClose.on("click", function (event) {
-//         menuWrapper.removeClass(openedMenu);
-//         menuBtn.removeClass(openedMenu);
-//         $html.removeClass(overflowHidden);
-//         $html.removeClass("open_menu");
-//     });
-//
-//     $(document).on("click touchstart", function (e) {
-//         if (
-//             $(e.target).closest(menuBtn).length ||
-//             $(e.target).closest(menuWrapper).length
-//         )
-//             return;
-//         if (menuBtn.hasClass(openedMenu)) {
-//             menuWrapper.removeClass(openedMenu);
-//             menuBtn.removeClass(openedMenu);
-//             $html.removeClass(overflowHidden);
-//             $html.removeClass("open_menu");
-//         }
-//     });
-//
-//     $('ul li a[href="#partners"]').on("click", function (e) {
-//         e.preventDefault();
-//         menuWrapper.removeClass(openedMenu);
-//         menuBtn.removeClass(openedMenu);
-//         $html.removeClass(overflowHidden);
-//         $html.removeClass("open_menu");
-//         $("html, body").animate({
-//                 scrollTop: $($(this).attr("href")).offset().top - 100,
-//             },
-//             500
-//         );
-//     });
-//
-//     $('ul li a[href="#maps"]').on("click", function (e) {
-//         e.preventDefault();
-//         menuWrapper.removeClass(openedMenu);
-//         menuBtn.removeClass(openedMenu);
-//         $html.removeClass(overflowHidden);
-//         $html.removeClass("open_menu");
-//         $("html, body").animate({
-//                 scrollTop: $($(this).attr("href")).offset().top - 100,
-//             },
-//             500
-//         );
-//     });
-//
-//     function opendModal() {
-//         const modal = document.querySelector('.tt_form');
-//
-//         // document.body.classList.add('overflowHidden');
-//
-//         modal.classList.add('opened');
-//
-//         document.body.style.overflow = 'hidden';
-//     }
-//     // opendModal();
-//
-//     function showModalByScroll() {
-//         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
-//             opendModal();
-//             window.removeEventListener('scroll', showModalByScroll);
-//         }
-//     }
-//
-//     window.addEventListener('scroll', showModalByScroll);
-//
-// });
 
 
 $('.slider_inner').slick({
@@ -95,6 +9,25 @@ $('.slider_inner').slick({
     prevArrow: false,
     nextArrow: false
 });
+
+$('.history_cards').slick({
+    mobileFirst: true,
+    infinite: true,
+    slidesToShow: 1,
+    variableWidth: true,
+    adaptiveHeight: true,
+    // width: 420,
+    prevArrow: false,
+    nextArrow: false,
+    responsive: [
+        {
+            breakpoint: 767,
+            settings: "unslick"
+        }
+    ]
+});
+
+
 
 // $('.method_card').slick({
 //     dots: true,
