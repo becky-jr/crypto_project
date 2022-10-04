24d1e0fb9f4e17dc04870765413947c39abb1913
Bekzod <bekrov21@gmail.com>
2022-10-04 19:31:41 +0500

    possibly ready

--- js/script.js
+++ js/script.js
@@ -143,3 +143,91 @@ modalCloseConfident.forEach(btn => {
     })
 })
 
+
+$(function () {
+    var $html = $(document.documentElement),
+        menuBtn = $(".burger"),
+        menuWrapper = $(".menu_burger"),
+        menuClose = $(".menuClose"),
+        openedMenu = "opened",
+        overflowHidden = "oveflowHidden";
+
+
+
+    menuBtn.on("click", function (event) {
+        menuWrapper.toggleClass(openedMenu);
+        menuBtn.toggleClass(openedMenu);
+        $html.toggleClass(overflowHidden);
+        $html.toggleClass("open_menu");
+    });
+    menuClose.on("click", function (event) {
+        menuWrapper.removeClass(openedMenu);
+        menuBtn.removeClass(openedMenu);
+        $html.removeClass(overflowHidden);
+        $html.removeClass("open_menu");
+    });
+
+    $(document).on("click touchstart", function (e) {
+        if (
+            $(e.target).closest(menuBtn).length ||
+            $(e.target).closest(menuWrapper).length
+        )
+            return;
+        if (menuBtn.hasClass(openedMenu)) {
+            menuWrapper.removeClass(openedMenu);
+            menuBtn.removeClass(openedMenu);
+            $html.removeClass(overflowHidden);
+            $html.removeClass("open_menu");
+        }
+    });
+
+    $('ul li a[href="#partners"]').on("click", function (e) {
+        // e.preventDefault();
+        menuWrapper.removeClass(openedMenu);
+        menuBtn.removeClass(openedMenu);
+        $html.removeClass(overflowHidden);
+        $html.removeClass("open_menu");
+        $("html, body").animate({
+                scrollTop: $($(this).attr("href")).offset().top - 100,
+            },
+            500
+        );
+    });
+
+    $('ul li a[href="#maps"]').on("click", function (e) {
+        // e.preventDefault();
+        menuWrapper.removeClass(openedMenu);
+        menuBtn.removeClass(openedMenu);
+        $html.removeClass(overflowHidden);
+        $html.removeClass("open_menu");
+        $("html, body").animate({
+                scrollTop: $($(this).attr("href")).offset().top - 100,
+            },
+            500
+        );
+    });
+
+    function openedModal() {
+        const modal = document.querySelector('.tt_form');
+        console.log(modal)
+
+        // document.body.classList.add('overflowHidden');
+        if (modal !== null) {
+            modal.classList.add('opened');
+
+            document.body.style.overflow = 'hidden';
+        }
+
+    }
+    // opendModal();
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openedModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+});
